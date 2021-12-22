b4349c06441e9dc3a480bc1d33795b7d730d60b7
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-22 10:29:52 -0800

    add views for basic page functionality
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- app.rb
+++ app.rb
@@ -7,3 +7,26 @@ also_reload('lib/**/*.rb')
 require 'pg'
 
 DB = PG.connect({:dbname => "train_system"})
+
+get('/') do
+  erb(:index)
+end
+
+get('/trains') do
+  @trains = Train.all
+  erb(:trains)
+end
+
+get('/cities') do
+  @cities = City.all
+  erb(:cities)
+end
+
+get('/trains/new') do
+  erb(:new_train)
+end
+
+post('/trains') do
+  name = params[:]
+
+end
