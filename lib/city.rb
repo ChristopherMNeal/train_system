42f80d1c5088540c6c14b5b468e5d72bc6740ac9
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-22 09:33:32 -0800

    add methods to city.rb
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- lib/city.rb
+++ lib/city.rb
@@ -1,5 +1,53 @@
 require('pry')
 
 class City
-  attr_accessor(:)
+  attr_accessor :id,:name, :station_name
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @station_name = attributes.fetch(:station_name)
+    @id = attributes.fetch(:id)
+  end
+  
+  def self.all
+    returned_cities = DB.exec("SELECT * FROM cities;")
+    cities = []
+    returned_cities.each() do |city|
+      name = city.fetch("name")
+      station_name = city.fetch("station_name")
+      id = city.fetch("id").to_i
+      cities.push(City.new({:name => name, :station_name => station_name, :id => id}))
+    end
+    cities
+  end
+  
+  def save
+    result = DB.exec("INSERT INTO cities (name, station_name) VALUES ('#{@name}', '#{@station_name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(city_to_compare)
+    (self.name == city_to_compare.name) && (self.station_name == city_to_compare.station_name)
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM cities *;")
+  end
+  
+  def self.find(id)
+    city = DB.exec("SELECT * FROM cities WHERE id = #{id};").first
+    name = city.fetch("name")
+    station_name = city.fetch("station_name")
+    id = city.fetch("id")
+    City.new({:name => name, :station_name => station_name, :id => id})
+  end
+
+  def update(name, station_name)
+    @name = name
+    @station_name = station_name
+    DB.exec("UPDATE cities SET name = '#{@name}', station_name = '#{@station_name}' WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM cities WHERE id = #{@id};")
+  end
 end
---
+++ spec/city_spec.rb
@@ -0,0 +1,7 @@
+require 'city'
+require 'spec_helper'
+
+describe('#City') do
+  describe("#initialize")
+  end
+end
