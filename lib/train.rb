182b6cb55a1f314ba4b140561b79b60f68e28520
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-22 10:14:29 -0800

    add add_stop method to train.rb and pass test
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- lib/train.rb
+++ lib/train.rb
@@ -1,1 +1,58 @@
 require("pry")
+
+class Train
+  attr_accessor :id, :name
+  
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+  
+  def self.all
+    returned_trains = DB.exec("SELECT * FROM trains;")
+    trains = []
+    returned_trains.each() do |train|
+      name = train.fetch("name")
+      id = train.fetch("id").to_i
+      trains.push(Train.new({:name => name, :id => id}))
+    end
+    trains
+  end
+  
+  def save
+    result = DB.exec("INSERT INTO trains (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(train_to_compare)
+    self.name == train_to_compare.name
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM trains *;")
+  end
+  
+  def self.find(id)
+    train = DB.exec("SELECT * FROM trains WHERE id = #{id};").first
+    name = train.fetch("name")
+    id = train.fetch("id")
+    Train.new({:name => name, :id => id})
+  end
+
+  def update(attributes)
+    
+  end
+  
+  def add_stop(attributes)
+    city_name = attributes.fetch(:city_name)
+    time = attributes.fetch(:time)
+    city_id = DB.exec("SELECT id FROM cities WHERE name = '#{city_name}';").first
+    if city_id != nil
+      DB.exec("INSERT INTO stops (train_id, city_id, time) VALUES (#{@id}, #{city_id["id"].to_i}, '#{time}');") 
+    end
+  end
+
+  def delete
+    DB.exec("DELETE FROM trains WHERE id = #{@id};")
+  end
+end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,2 +1,50 @@
 require("rspec")
 require("train")
+
+describe('#Train') do
+  describe("#initialize") do
+    it('should create new object train') do
+      train1 = Train.new({:name => "Amtrak Portland-Seattle", :id => nil})
+      expect(train1.name).to(eq("Amtrak Portland-Seattle"))
+    end
+  end
+
+  describe("#save") do
+    it('should save object train to database trains') do
+      train1 = Train.new({:name => "Amtrak Portland-Seattle", :id => nil})
+      train1.save
+      expect(Train.all).to(eq([train1]))
+    end
+  end
+
+  # describe("#update") do
+  #   it('should update train station name from Amtrak Seattle-Vancouver to Amtrak Portland-Seattle') do
+  #     train1 = City.new({:name => "Amtrak Portland-Seattle", :id => nil})
+  #     city1.save
+  #     city1.update("Amtrak Portland-LA")
+  #     expect(City.all[0].station_name).to(eq("Union Station"))
+  #   end
+  # end
+
+  describe(".find") do
+    it('should find a city by id') do
+      train1 = Train.new({:name => "Amtrak Portland-Seattle", :id => nil})
+      train1.save
+      train2 = Train.new({:name => "Amtrak Portland-LA", :id => nil})
+      train2.save
+      expect(Train.find(train1.id)).to(eq(train1))
+    end
+  end
+  
+  describe("#add_stop") do
+    it('should find a city by id') do
+      train1 = Train.new({:name => "Amtrak Portland-Seattle", :id => nil})
+      train1.save
+      city1 = City.new({:name => "Portland", :station_name => "Union Station", :id => nil})
+      city1.save
+      train1.add_stop({:city_name => "Portland", :time => '2021-12-21 08:00:00'})
+      result = DB.exec("SELECT * FROM stops;")
+      expect(result.first).to(eq(1))
+    end
+  end
+end
